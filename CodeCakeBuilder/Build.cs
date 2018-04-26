abe9cddccfd1e73e72444879a5e9f11084e92a38
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-04-26 18:21:41 +0200

    Updated Repository.xml and nuget.config for blank dev branch.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -82,6 +82,8 @@ namespace CodeCake
 
             Task( "Unit-Testing" )
                 .IsDependentOn( "Build" )
+                .WithCriteria( () => Cake.InteractiveMode() == InteractiveMode.NoInteraction
+                                     || Cake.ReadInteractiveOption( "RunUnitTests", "Run Unit Tests?", 'Y', 'N' ) == 'Y' )
                 .Does( () =>
                 {
                     StandardUnitTests( globalInfo.BuildConfiguration, projects.Where( p => p.Name.EndsWith( ".Tests" ) ) );
