c0855d33099d052c78627a2697a1657a8b05332f
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-05-03 17:01:43 +0200

    Restore default ctor.

--- CK.ActivityMonitor/Client/ActivityMonitorConsoleClient.cs
+++ CK.ActivityMonitor/Client/ActivityMonitorConsoleClient.cs
@@ -10,10 +10,21 @@ namespace CK.Core
     /// </summary>
     public sealed class ActivityMonitorConsoleClient : ActivityMonitorTextWriterClient
     {
+        
         /// <summary>
         /// Initializes a new <see cref="ActivityMonitorConsoleClient"/>.
         /// </summary>
-        public ActivityMonitorConsoleClient( bool useErrorStream = false )
+        public ActivityMonitorConsoleClient()
+            : base( Console.Out.Write )
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="ActivityMonitorConsoleClient"/> that outputs
+        /// to the standard output or the error output.
+        /// </summary>
+        /// <param name="useErrorStream">True to output logs to standard error stream, false for standard output.</param>
+        public ActivityMonitorConsoleClient( bool useErrorStream )
             : base( useErrorStream ? (Action<string>)Console.Error.Write : Console.Out.Write )
         {
         }
