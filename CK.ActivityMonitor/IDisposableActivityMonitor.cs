488a31a07ed132a7503fdaf93cd1fab3f6794811
Olivier Spinelli <olivier.spinelli@signature-code.com>
2019-10-28 10:36:19 +0100

    New IDisposableActivityMonitor.

---
+++ CK.ActivityMonitor/IDisposableActivityMonitor.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CK.Core
+{
+
+    /// <summary>
+    /// A diposable <see cref="IActivityMonitor"/>.
+    /// This is typically used when monitors must be reusable from a pool: the Dispose method
+    /// returns the object to the pool.
+    /// </summary>
+    public interface IDisposableActivityMonitor : IActivityMonitor, IDisposable
+    {
+    }
+}
+
+
+
