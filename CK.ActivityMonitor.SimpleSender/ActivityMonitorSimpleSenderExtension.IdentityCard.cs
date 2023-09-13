bc6daacdb8359adc09c59fca2c498d01653db2fe
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-09-13 09:49:44 +0200

    Opens the check identity information API.
    Better handling of the AddIdentityInformation for invalid information.

--- CK.ActivityMonitor.SimpleSender/ActivityMonitorSimpleSenderExtension.IdentityCard.cs
+++ CK.ActivityMonitor.SimpleSender/ActivityMonitorSimpleSenderExtension.IdentityCard.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System;
 using System.Reflection.Emit;
+using System.Formats.Asn1;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CK.Core
 {
@@ -11,19 +13,19 @@ namespace CK.Core
         /// This sends an unfiltered information line tagged with <see cref="IdentityCard.IdentityCardUpdate"/>.
         /// <see cref="IdentityCard.Pack(string, string)"/> is used to pack the text.
         /// <para>
-        /// The characters 0 to 8 (NUl, SOH, STX, ETX, EOT, ENQ, ACK, BEL, BSP) are invalid in a key and in a value.
-        /// </para> 
-        /// <para>
-        /// A key cannot contain newlines: the Unicode Standard, Sec. 5.8, Recommendation R4 and Table 5-2 state
-        /// that the CR, LF, CRLF, NEL, LS, FF, and PS sequences are considered newline functions.
+        /// If the key or the value fails <see cref="IdentityCard.TryValidateIdentityInformation(string, string, out string?)"/>
+        /// then the error message is logged.
         /// </para>
         /// </summary>
         /// <param name="logger">This logger.</param>
-        /// <param name="key">The identity key. Must not be null, empty or white space or contain any line delimiter.</param>
-        /// <param name="value">The identity information. Must not be empty.</param>
+        /// <param name="key">The identity key.</param>
+        /// <param name="value">The identity key value to append.</param>
         public static void AddIdentityInformation( this IActivityLineEmitter logger, string key, string value )
         {
-            logger.UnfilteredLog( LogLevel.Info | LogLevel.IsFiltered, IdentityCard.IdentityCardUpdate, IdentityCard.Pack( key, value ), null );
+            if( IdentityCard.CheckIdentityInformation( key, value, logger ) )
+            {
+                logger.UnfilteredLog( LogLevel.Info | LogLevel.IsFiltered, IdentityCard.IdentityCardUpdate, IdentityCard.Pack( key, value ), null );
+            }
         }
 
         /// <summary>
@@ -51,14 +53,19 @@ namespace CK.Core
             /// <summary>
             /// Packs a (name, value) in a single string separated by STX (0x02) character
             /// that can be sent with the <see cref="IdentityCardUpdate"/> tag. The key and value must be valid:
-            /// see <see cref="CkeckIdentityInformation"/>
+            /// see <see cref="TryValidateIdentityInformation(string, string, out string?)"/>
             /// </summary>
             /// <param name="key">The identity key. Must not be null, empty or white space or contain any line delimiter.</param>
             /// <param name="value">The identity information. Must not be empty.</param>
             /// <returns>The packed string.</returns>
             public static string Pack( string key, string value )
             {
-                CkeckIdentityInformation( key, value );
+                CheckIdentityInformation( key, value, null );
+                return DoPack( key, value );
+            }
+
+            static string DoPack( string key, string value )
+            {
                 return String.Create( key.Length + 1 + value.Length, (key, value), ( s, i ) =>
                 {
                     i.key.CopyTo( s );
@@ -69,7 +76,25 @@ namespace CK.Core
             }
 
             /// <summary>
-            /// Throws argument exceptions if key or value of the identity information are invalid.
+            /// Calls <see cref="TryValidateIdentityInformation(string, string, out string?)"/> and logs the error message in
+            /// the <paramref name="logger"/> or throws an argument exception
+            /// </summary>
+            /// <param name="key">The identity key to check.</param>
+            /// <param name="value">The identity information. Must not be empty.</param>
+            /// <param name="logger">The logger for the error. When null an ArgumentException is thrown.</param>
+            public static bool CheckIdentityInformation( string key, string value, IActivityLineEmitter? logger )
+            {
+                if( !TryValidateIdentityInformation( key, value, out var message ) )
+                {
+                    if( logger == null) Throw.ArgumentException( "identity", message );
+                    logger.Error( message );
+                    return false;
+                }
+                return true;
+            }
+
+            /// <summary>
+            /// Checks whether arguments key and value of the identity information are valid.
             /// <para>
             /// The characters 0 to 8 (NUl, SOH, STX, ETX, EOT, ENQ, ACK, BEL, BSP) are invalid in a key and in a value.
             /// </para> 
@@ -80,15 +105,29 @@ namespace CK.Core
             /// </summary>
             /// <param name="key">The identity key. Must not be null, empty or white space or contain any line delimiter.</param>
             /// <param name="value">The identity information. Must not be empty.</param>
-            public static void CkeckIdentityInformation( string key, string value )
+            /// <param name="errorMessage">When invalid, an error message is provided.</param>
+            /// <returns>True on success, false otherwise.</returns>
+            public static bool TryValidateIdentityInformation( string key, string value, [NotNullWhen(false)] out string? errorMessage )
             {
-                Throw.CheckNotNullOrEmptyArgument( key );
-                Throw.CheckNotNullOrEmptyArgument( value );
-                Throw.CheckArgument( "Invalid newline character in identity key.", key.AsSpan().IndexOfAny( "\r\n\f\u0085\u2028\u2029" ) < 0 );
-                Throw.CheckArgument( "First 8 characters (NUl, SOH, STX, ETX, EOT, ENQ, ACK, BEL, BSP) cannot appear.",
-                                        key.AsSpan().IndexOfAny( "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008" ) < 0 );
-                Throw.CheckArgument( "First 8 characters (NUl, SOH, STX, ETX, EOT, ENQ, ACK, BEL, BSP) cannot appear.",
-                                        value.AsSpan().IndexOfAny( "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008" ) < 0 );
+                static string Add( string? current, string message ) => current != null
+                                                                        ? $"{current}{Environment.NewLine}{message}"
+                                                                        : message;
+                errorMessage = null;
+                if( string.IsNullOrEmpty( key ) ) errorMessage = Add( errorMessage, $"Invalid empty identity key." );
+                if( string.IsNullOrEmpty( value ) ) errorMessage = Add( errorMessage, $"Invalid empty value for identity key: '{key}'." );
+                if( key.AsSpan().IndexOfAny( "\r\n\f\u0085\u2028\u2029" ) >= 0 )
+                {
+                    errorMessage = Add( errorMessage, $"Invalid newline character in identity key: '{key}'." );
+                }
+                if( key.AsSpan().IndexOfAny( "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008" ) >= 0 )
+                {
+                    errorMessage = Add( errorMessage, $"Invalid characters in identity key: '{key}'. First 8 characters (NUl, SOH, STX, ETX, EOT, ENQ, ACK, BEL, BSP) cannot appear." );
+                }
+                if( value.AsSpan().IndexOfAny( "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008" ) >= 0 )
+                {
+                    errorMessage = Add( errorMessage, $"Invalid characters in value for identity key: '{key}'. First 8 characters (NUl, SOH, STX, ETX, EOT, ENQ, ACK, BEL, BSP) cannot appear." );
+                }
+                return errorMessage == null;
             }
         }
     }
